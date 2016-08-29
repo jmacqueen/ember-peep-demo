da462f527ad81e1ec0573f43c31f58b73a19e4a4
Jonathan MacQueen <jmacqueen@echo360.com>
2016-08-29 12:12:33 -0400

    fancy flash messages

--- app/routes/auth/login.js
+++ app/routes/auth/login.js
@@ -4,12 +4,24 @@ const { inject } = Ember
 
 export default Ember.Route.extend({
   session: inject.service(),
+  flashMessages: inject.service(),
+
   actions: {
     doLogin() {
       const user = this.get('currentModel')
       this.get('session').authenticate(
         'authenticator:peepchat', user.email, user.password
-      )
+      ).then(() => {
+        this.get('flashMessages').success('Logged in!')
+      }).catch((response) => {
+        const { errors } = response
+        if (errors.mapBy('code').indexOf(401) >= 0) {
+          this.get('flashMessages')
+            .danger('There was a problem with your password, please try again.')
+        } else {
+          this.get('flashMessages').danger('Server Error')
+        }
+      })
     }
   },
   model() {
